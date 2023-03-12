c8ddea813688df7014606a1c5802b25b1fcbe3f6
ptrxyz <ptrxyz@gmail.com>
2023-03-12 22:36:20 +0100

    Update extconf.rb

--- ext/rdkit_chem/extconf.rb
+++ ext/rdkit_chem/extconf.rb
@@ -62,7 +62,7 @@ Dir.chdir build_dir do
   puts 'Configuring RDKit'
 
   cmake = "#{ld_path} cmake #{src_dir} -DRDK_INSTALL_INTREE=OFF " \
-          "-DCMAKE_INSTALL_PREFIX=#{install_dir}" \
+          "-DCMAKE_INSTALL_PREFIX=#{install_dir} " \
           '-DCMAKE_BUILD_TYPE=Release -DRDK_BUILD_CPP_TESTS=OFF -DRDK_BUILD_PYTHON_WRAPPERS=OFF ' \
           '-DRDK_BUILD_SWIG_WRAPPERS=ON -DRDK_BUILD_INCHI_SUPPORT=OFF ' \
           '-DBoost_NO_BOOST_CMAKE=ON'
